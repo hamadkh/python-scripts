bf544031e4df1ead8164a0e78dcce1e6fa1edb21
hamad Khawaja <khawajahamad217@gmail.com>
2017-04-18 18:29:37 -0400

    server for local host

---
+++ server.py
@@ -0,0 +1,20 @@
+#ps -fA | grep python
+#sudo kill -9 PID
+#hamad
+#web.py running the server 
+
+import web
+
+urls = (
+    '/(.*)', 'hello'
+)
+app = web.application(urls, globals())
+
+class hello:
+    def GET(self, name):
+        if not name:
+            name = 'World'
+        return 'Hello, ' + name + '!'
+
+if __name__ == "__main__":
+    app.run()
